46879a2f07299a9b9f36bdaf258427b504ed6f3d
brunaporato <brunaporato@gmail.com>
2024-05-17 06:38:40 -0300

    config: command prebuild

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+import { OneSignal } from 'react-native-onesignal';
+
+OneSignal.initialize("1a11c73c-60b0-456b-bbde-ac50d5ff8f9c");
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
